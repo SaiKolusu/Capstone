e6bf32b6a1eab1bbbe1dafec32cfba9099456fa0
SaiKolusu <skolusu@saintpeters.edu>
2016-11-30 23:42:42 -0500

    Project Code Update

---
+++ R1.js
@@ -0,0 +1,39 @@
+var heatmapDatas1 = [
+				new google.maps.LatLng(40.7597,-73.9917),
+				new google.maps.LatLng(40.7615,-73.9978),
+				new google.maps.LatLng(40.7608,-73.9983),
+				new google.maps.LatLng(40.7577,-74.0006),
+				new google.maps.LatLng(40.7551,-74.0025),
+				new google.maps.LatLng(40.7502,-74.0061)
+			];
+	
+var heatmapDatas2 = heatmapDatas = [
+			new google.maps.LatLng(40.7052, -74.0166),
+			new google.maps.LatLng(40.7094, -73.9943),
+			new google.maps.LatLng(40.7106, -73.9847),
+			new google.maps.LatLng(40.7153, -73.9763),
+			new google.maps.LatLng(40.7295, -73.9721),
+			new google.maps.LatLng(40.7475, -73.9688),
+			new google.maps.LatLng(40.7498, -73.9722),
+			new google.maps.LatLng(40.7528, -73.9793),
+			new google.maps.LatLng(40.7584, -73.9926),
+			new google.maps.LatLng(40.7597, -73.9917)
+
+
+		];	
+var heatmapDatas3 = [
+			new google.maps.LatLng(40.7429, -74.004),
+			new google.maps.LatLng(40.7441, -74.0068),
+			new google.maps.LatLng(40.7452, -74.0023),
+			new google.maps.LatLng(40.7527, -73.9968),
+			new google.maps.LatLng(40.7528, -73.9967),
+			new google.maps.LatLng(40.7541, -73.9958),
+			new google.maps.LatLng(40.7547, -73.9954),
+			new google.maps.LatLng(40.7553, -73.9949),
+			new google.maps.LatLng(40.7559, -73.9945),
+			new google.maps.LatLng(40.7565, -73.994),
+			new google.maps.LatLng(40.7574, -73.9934),
+			new google.maps.LatLng(40.7584, -73.9926),
+			new google.maps.LatLng(40.7597, -73.9917)
+
+		];
